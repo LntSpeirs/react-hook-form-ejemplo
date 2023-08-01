3b3698ef0d6907a16be9610698df94b37c560eec
Jaime Quilez Ramirez <jaimequilez2601@yahoo.es>
2023-08-01 15:36:19 +0200

    Validacion inicial

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import { useForm } from "react-hook-form";
 
 function App() {
-  const { register, handleSubmit } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
   //Use form ya tiene su propia funcion handlesubmit
   /* El `const manejarEnvio` es una función que se crea utilizando la función `handleSubmit` de la
   biblioteca `react-hook-form`. Toma una función de devolución de llamada como argumento, que se
@@ -11,6 +15,14 @@ function App() {
     console.log("Formulario enviado");
     console.log(data);
   });
+
+  /*
+  Formstate captura todos los valores, y si alguno falla, 
+  crea un objeto error que indica cual es el que falla
+  */
+  console.log("ERRORES:");
+  console.table(errors);
+
   return (
     <form onSubmit={manejarEnvio}>
       <label htmlFor="nombre">Nombre:</label>
@@ -22,18 +34,44 @@ function App() {
           maxLength: { value: 50 }, //El valor máximo permitido para este campo
         })}
       />
+      {errors.nombre?.type === "required" && (
+        <span>El nombre es obligatorio.</span>
+      )}
+      {errors.nombre?.type === "minLength" && (
+        <span>El nombre debe tener al menos tres caracteres de longitud.</span>
+      )}
 
       <label htmlFor="correo">Correo:</label>
-      <input type="email" {...register("email")} />
+      <input
+        type="email"
+        {...register("email", {
+          required: true, //Este campo debe ser requerido
+        })}
+      />
 
       <label htmlFor="passWord">Password:</label>
-      <input type="passWord" {...register("password")} />
+      <input
+        type="passWord"
+        {...register("password", {
+          required: true, //Este campo debe ser requerido
+        })}
+      />
 
       <label htmlFor="ConfirmarPassWord">Confirmar Password:</label>
-      <input type="passWord" {...register("confirmarPassword")} />
+      <input
+        type="passWord"
+        {...register("confirmarPassword", {
+          required: true, //Este campo debe ser requerido
+        })}
+      />
 
       <label htmlFor="fechaNacimiento">Fecha de nacimiento:</label>
-      <input type="date" {...register("fechaNacimiento")} />
+      <input
+        type="date"
+        {...register("fechaNacimiento", {
+          required: true, //Este campo debe ser requerido
+        })}
+      />
 
       <label htmlFor="pais">País:</label>
       <select {...register("pais")}>
@@ -47,7 +85,12 @@ function App() {
       <input type="file" {...register("foto")} />
 
       <label htmlFor="terminos">Acepto los términos y condiciones</label>
-      <input type="checkbox" {...register("terminos")} />
+      <input
+        type="checkbox"
+        {...register("terminos", {
+          required: true, //Este campo debe ser requerido
+        })}
+      />
 
       <button>Enviar</button>
     </form>
